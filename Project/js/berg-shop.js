f14e6a4498f0d3d27169d10641be3f9220dc9d9b
Joél Friedrich <joel@jfriedrich.net>
2020-08-06 21:03:39 +0200

    change: - Add product pages - Add modal dialog

--- Project/js/berg-shop.js
+++ Project/js/berg-shop.js
@@ -14,3 +14,22 @@ function switchImage(image) {
     expandImg.src = image.src;
     expandImg.parentElement.style.display = "block";
 }
+
+/* Modal Dialog */
+function showModal(image) {
+    const target = image.dataset.target;
+    const modal = document.getElementById(target);
+
+    const img = document.getElementById('modal-image');
+    img.setAttribute('src', image.src);
+
+    modal.style.display = "block";
+}
+
+/* Close Button */
+function closeModal(close) {
+    const target = close.parentElement.dataset.target;
+    const modal = document.getElementById(target);
+
+    modal.style.display = "none";
+}
