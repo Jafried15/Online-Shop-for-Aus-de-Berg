bf1f6be6e92c63e747cf304e33cab122f0968cb8
Joél Friedrich <joel@jfriedrich.net>
2020-11-25 20:46:34 +0100

    Change Handling of Cart JS

--- Project/js/cart.js
+++ Project/js/cart.js
@@ -10,12 +10,13 @@
 
 'use strict';
 
-let cart = [];
+let cart = (JSON.parse(localStorage.getItem('cart')) || []);
 
 const cartDOM = document.querySelector('.cart-table');
 
-const addToCartButtons = document.querySelectorAll('[data-action="ADD_TO_CART"]');
-addToCartButtons.forEach(addItemToCart => {
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
+
+addToCartButtonsDOM.forEach(addItemToCart => {
     addItemToCart.addEventListener('click', () => {
         const productDOM = document.getElementById(addItemToCart.getAttribute("data-target"));
         const product = {
@@ -28,61 +29,107 @@ addToCartButtons.forEach(addItemToCart => {
 
         const isInCart = (cart.filter(cartItem => (cartItem.article_number === product.article_number)).length > 0);
         if (!isInCart) {
-            cartDOM.insertAdjacentHTML('beforeend', `
-                <tr class="cart-item" data-article_number="${product.article_number}">
-                    <td><img class="cart-item-image" src="${product.image}" alt="${product.name}">
-                        <h3 class="cart-item-name">${product.name}</h3></td>
-                    <td><h3 class="cart-item-price">${product.price}</h3></td>
-                    <td>
-                        <button class="quantity-btn" data-action="DECREASE_ITEM">&minus;</button>
-                        <h3 class="cart-item-count">${product.count}</h3>
-                        <button class="quantity-btn" data-action="INCREASE_ITEM">&plus;</button>
-                        <button class="quantity-btn" data-action="REMOVE_ITEM">&times;</button>
-                    </td>
-                </tr>
-            `);
-
+            increaseItemCountAdd(product, product.count);
             cart.push(product);
-
-            const cartItemsDOM = cartDOM.querySelectorAll('.cart-item');
-            cartItemsDOM.forEach(cartItemDOM => {
-                if (cartItemDOM.getAttribute('data-article_number') === product.article_number) {
-                    cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => {
-                        cart.forEach(cartItem => {
-                            if (cartItem.article_number === product.article_number) {
-                                if (cartItem.article_number === "1.5" && parseInt(cartItem.count) < 100) {
-                                    cartItem.count = parseInt(cartItem.count) + 20;
-                                } else {
-                                    cartItem.count = parseInt(cartItem.count) + 25;
-                                }
-                                cartItemDOM.querySelector('.cart-item-count').innerText = cartItem.count;
-                            }
-                        })
-                    });
-
-                    cartItemDOM.querySelector('[data-action="DECREASE_ITEM"]').addEventListener('click', () => {
-                        cart.forEach(cartItem => {
-                            if (cartItem.article_number === product.article_number && cartItem.count > 25) {
-                                if (cartItem.article_number === "1.5" && parseInt(cartItem.count) <= 100) {
-                                    cartItem.count = parseInt(cartItem.count) - 20;
-                                } else {
-                                    cartItem.count = parseInt(cartItem.count) - 25;
-                                }
-                                cartItemDOM.querySelector('.cart-item-count').innerText = cartItem.count;
-                            }
-                        })
-                    });
-
-                    cartItemDOM.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', () => {
-                        cart.forEach(cartItem => {
-                            if (cartItem.article_number === product.article_number) {
-                                cartItemDOM.remove();
-                                cart = cart.filter(cartItem => cartItem.article_number !== product.article_number);
-                            }
-                        })
-                    });
-                }
-            })
+            localStorage.setItem('cart', JSON.stringify(cart));
+        } else {
+            cart.push(product);
+            localStorage.setItem('cart', JSON.stringify(cart));
         }
     })
 });
+
+function increaseItemCountAdd(product, count) {
+    cart.forEach(cartItem => {
+        if (cartItem.article_number === product.article_number) {
+            cartItem.count = parseInt(cartItem.count) + count;
+            //cartItemDOM.querySelector('.cart-item-count').innerText = cartItem.count;
+            localStorage.setItem('cart', JSON.stringify(cart));
+
+        }
+    })
+}
+
+function removeItem(product, cartItemDOM) {
+    cart.forEach(cartItem => {
+        if (cartItem.article_number === product.article_number) {
+            cartItemDOM.remove();
+            cart = cart.filter(cartItem => cartItem.article_number !== product.article_number);
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    })
+}
+
+function showCart() {
+    if (cart.length === 0) {
+        document.getElementById("empty").style.display = "block";
+        document.getElementById("basket-icon").style.display = "none";
+        document.getElementById("filled").style.display = "none";
+        document.getElementById("step-one").style.display = "none";
+    } else {
+        cart = JSON.parse(localStorage.getItem('cart'));
+        document.getElementById("empty").style.display = "none";
+        document.getElementById("basket-icon").style.display = "block";
+        document.getElementById("filled").style.display = "block";
+        document.getElementById("step-one").style.display = "block";
+        cart.forEach(cartItem => {
+            addToDOM(cartItem);
+        });
+    }
+}
+
+function addToDOM(product) {
+    cartDOM.insertAdjacentHTML('beforeend', `
+            <tr class="cart-item" data-article_number="${product.article_number}">
+                <td><img class="cart-item-image" src="${product.image}" alt="${product.name}">
+                     <h3 class="cart-item-name">${product.name}</h3></td>
+                <td><h3 class="cart-item-price">${product.price}</h3></td>
+                <td>
+                    <button class="quantity-btn" data-action="DECREASE_ITEM">&minus;</button>
+                    <h3 class="cart-item-count">${product.count}</h3>
+                    <button class="quantity-btn" data-action="INCREASE_ITEM">&plus;</button>
+                    <button class="quantity-btn" data-action="REMOVE_ITEM">&times;</button>
+                </td>
+            </tr>
+        `);
+    addActionsToButtons(product);
+}
+
+function addActionsToButtons(product) {
+    const cartItemsDOM = cart.querySelectorAll('.cart-item');
+    cartItemsDOM.forEach(cartItemDOM => {
+        if (cartItemDOM.getAttribute('data-article_number') === product.article_number) {
+            cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => increaseItemCountCart(product));
+            cartItemDOM.querySelector('[data-action="DECREASE_ITEM"]').addEventListener('click', () => decreaseItemCount(product, cartItemDOM));
+            cartItemDOM.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', () => removeItem(product, cartItemDOM));
+        }
+    })
+}
+
+function increaseItemCountCart(product) {
+    cart.forEach(cartItem => {
+        if (cartItem.article_number === product.article_number) {
+            if (cartItem.article_number === "1.5" && parseInt(cartItem.count) < 100) {
+                cartItem.count = parseInt(cartItem.count) + 20;
+            } else {
+                cartItem.count = parseInt(cartItem.count) + 25;
+            }
+            //cartItemDOM.querySelector('.cart-item-count').innerText = cartItem.count;
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    })
+}
+
+function decreaseItemCount(product) {
+    cart.forEach(cartItem => {
+        if (cartItem.article_number === product.article_number && cartItem.count > 25) {
+            if (cartItem.article_number === "1.5" && parseInt(cartItem.count) <= 100) {
+                cartItem.count = parseInt(cartItem.count) - 20;
+            } else {
+                cartItem.count = parseInt(cartItem.count) - 25;
+            }
+            //cartItemDOM.querySelector('.cart-item-count').innerText = cartItem.count;
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    });
+}
