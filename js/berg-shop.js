a8f5be861131e3d42fcad873b1c58f1a24217598
Joel Friedrich <joel.friedrich@sersoft.de>
2021-04-28 17:59:21 +0200

    Some changes

--- js/berg-shop.js
+++ js/berg-shop.js
@@ -183,13 +183,12 @@ paypal.Buttons({
     onApprove: function (data, actions) {
         // This function captures the funds from the transaction.
         return actions.order.capture().then(function (details) {
-            // This function shows a transaction success message to your buyer.
-            alert('Transaction completed by ' + details.payer.name.given_name);
+            window.location.href = 'Finish.html';
         });
     },
 }).render('#paypal-button-container');
 
 function clearStorage() {
     sessionStorage.clear();
-    clearCart()
+    clearCart();
 }
